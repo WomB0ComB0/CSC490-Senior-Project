9d8d9cd3640f9871c30ca47789b0f3868414cfcd
Mike Odnis <airwolf635@gmail.com>
2025-01-28 09:19:24 -0500

    Quick commit

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+(async() => {
+  const $: typeof import('bun').$ = await import('bun').then(m => m.$);
+  for (const i of Array.from({ length: 15 }).map((_, i) => i + 1))
+    await $`mkdir -p week-${i} && cd week-${i} && touch README.md`;
+})()
